7c7719e8fbde84936636e6f47ba5f10ff7a06a2b
Mandy Farrugia <deefarrugia2001@gmail.com>
2022-08-07 16:11:53 +0200

    Created constructors for each table mapped to a class.

--- AttendanceSystem/Domain/TypeEdit.cs
+++ AttendanceSystem/Domain/TypeEdit.cs
@@ -8,8 +8,48 @@ namespace Domain
 {
     public partial class Course
     {
-        public Course(string course)
+        public Course(string courseTitle)
         {
+            this.CourseTitle = CourseTitle;
+        }
+    }
+
+    public partial class Group
+    {
+        public Group(string name, int courseID)
+        {
+            this.Name = name;
+            this.CourseID = courseID;
+        }
+    }
+
+    public partial class Lesson
+    {
+        public Lesson(int groupID, DateTime dateTime, int teacherID)
+        {
+            this.GroupID = groupID;
+            this.DateTime = dateTime;
+            this.TeacherID = teacherID;
+        }
+    }
+
+    public partial class Student
+    {
+        public Student(string name, string surname, string email, int groupID)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.GroupID = groupID;
+        }
+    }
+
+    public partial class StudentAttendance 
+    {
+        public StudentAttendance(int lessonID, bool presence, int studentID)
+        {
+            this.LessonID = lessonID;
+            this.Presence = presence;
+            this.StudentID = studentID;
         }
     }
 

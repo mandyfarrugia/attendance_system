1ed9624e87cb9b5e0c769474fca0a196d539bf27
Mandy Farrugia <deefarrugia2001@gmail.com>
2022-08-09 23:06:26 +0200

    Removed unnecessary using directives.

--- AttendanceSystem/DataLayer/DL.cs
+++ AttendanceSystem/DataLayer/DL.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Data.Entity.Infrastructure;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
 
 namespace DataLayer
 {
